f1f657ddbb1e7d637d0f27e36c09b17de0a356e1
Andrew Lucero <al4991@nyu.edu>
2019-09-29 17:13:30 -0400

    Started work on Sidebar (w. Lyanne)

---
+++ app/js/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+
+
+
+const Sidebar = (props) => {
+    return (
+        <div className="sidebar">
+            <Button onClick={props.onClick} variant="primary">Add a Tinkamo!</Button>
+        </div>
+    )
+}
+export default Sidebar;
--- app/js/index.jsx
+++ app/js/index.jsx
@@ -1,26 +1,38 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
+import Tinkamo from "@musedlab/tinkamod/web/tinkamo"; 
+import Sidebar from "./Sidebar"; 
+
+
+let tinkamo_ = new Tinkamo(); 
 
 function TinkamoEditor() {
   const [code, setCode] = useState("");
-
+  const [tinkamo, setTinkamo] = useState(tinkamo_); 
+  
+  const onClick = () => tinkamo.connect();
+  
   return (
     <div>
       <h1>Tinkamo Editor</h1>
       <textarea value={code} onChange={e => setCode(e.target.value)} />
       <pre>{code}</pre>
-      <TinkaList tinkamos={[]} />
+      <TinkaList tinkamos={tinkamo.getTinkamoList()} />
+      <Sidebar onClick={() => onClick()}/>
     </div>
   );
 }
 
 function TinkaList({ tinkamos }) {
   return (
-    <ul>
-      {tinkamos.map(tinka => (
-        <li>{tinka.id}</li>
-      ))}
-    </ul>
+    <div> 
+      Connected Tinkamos
+      <ul>
+        {tinkamos.map(tinka => (
+          <li>{tinka.id}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
