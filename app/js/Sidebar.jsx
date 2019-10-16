27e43a6bbbc3262867c3c3e5e77cd1c016c86dfd
lyannel <ll3325@nyu.edu>
2019-10-16 15:21:11 -0400

    Rendered simple sidebar and installed codemirror2 package

--- app/js/Sidebar.jsx
+++ app/js/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
+import './Sidebar.css';
+
 
 const Sidebar = (props) => {
     return (
         <div className="sidebar">
-            <Button onClick={props.onClick} variant="primary">Add a Tinkamo!</Button>
+            <Button onClick={props.onClick} variant="primary"> + New Tinka</Button>
         </div>
     )
 }
